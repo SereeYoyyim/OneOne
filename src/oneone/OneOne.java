bb4a9cdf428e9728f0e67c88966f0f8f9b6be206
Administrator <Administrator@10.240.19.59>
2014-05-30 15:53:58 +0700

    งาน OneOne

---
+++ src/oneone/OneOne.java
@@ -0,0 +1,14 @@
+package oneone;
+// /*prorrams need
+/*comments */
+public class OneOne {
+    private static String aString;
+
+    public static void main(String[] args) {
+       
+    aString = "Well!";
+    String OneOne= "Now!";
+    System.out.println("Isn't this an interesting program");
+    System.out.println("Answer yes or no!");
+    }
+    }
